e4e8f57c7a82c1b43fbb8265dc4df0c60eb4b77e
Jose Andres Hernandez Ross <joseandreshernandezross@gmail.com>
2019-01-09 14:45:38 -0430

    Cambios hechos

--- resources/views/modals/modal.blade.php
+++ resources/views/modals/modal.blade.php
@@ -2,7 +2,7 @@
 
    <!-- Large Size -->
             <div class="modal fade" id="largeModal" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-lg" role="document" style="width: 75%;">
+                <div class="modal-dialog modal-lg" role="document" style="width: 95%;">
                     <div class="modal-content">
                         <div class="modal-header">
                           <div class="row">
