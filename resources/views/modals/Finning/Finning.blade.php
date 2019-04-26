4703e115d62699911accb326f25ae2374f5c2814
Jose Hernandez <andres.hernandez@techlatam.la>
2019-04-26 08:05:12 -0400

    Finning Anadido

---
+++ resources/views/modals/Finning/Finning.blade.php
@@ -0,0 +1,289 @@
+<button type="button" class="btn btn-default waves-effect m-r-20 display-modal" data-toggle="modal" data-target="#largeModal" style="display: none"></button>
+<!-- Large Size -->
+<div class="modal fade" id="largeModal" tabindex="-1" role="dialog">
+   <div class="modal-dialog modal-lg" role="document" style="width: 95%;margin-top: 2%;">
+      <div class="modal-content">
+         <div class="modal-header">
+            <div class="row">
+               <div class="col-md-2">
+                  <h4 class="modal-title nombre-instalacion" id="largeModalLabel" >Finning</h4>
+               </div>
+               <div class="col-md-5">
+                  <h4 class="modal-title nombre-instalacion" id="largeModalLabel">Ultima Mendicion:{{--  {{$UltimaMedicion->mt_time}} --}}</h4>
+               </div>
+            </div>
+         </div>
+         <hr style=" color: black">
+         <div class="modal-body table-custom">
+            <div class="body">
+               <!-- Nav tabs -->
+               <ul class="nav nav-tabs tab-nav-right" role="tablist">
+                  <li role="presentation" class="active"><a href="#home" data-toggle="tab">Panel</a></li>
+                  <li role="presentation" id="parametros"><a href="#profile" data-toggle="tab">Parametros</a></li>
+               </ul>
+               <!-- Tab panes -->
+               <div class="tab-content">
+                  <div role="tabpanel" class="tab-pane fade in active" id="home">
+                     <div class="col-md-4">
+                       <div class="body table-responsive">
+                           <table class="table sicut-table-bordered sicut-modal-table1">
+                             <thead>
+                                 </thead><caption scope="row" class="sicut-tabla-titulo">Pozo nave 4</caption>
+                             <tbody>
+                                 <tr>
+                                     <th class="sicut-th">Nivel Bajo</th>
+                                     <td>13134</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel alto pozo</th>
+                                     <td>13328</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel alto</th>
+                                     <td>13084</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel alto TK</th>
+                                     <td>13182</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                       </div>
+                     </div>
+                     <div class="col-md-4">
+                       <div class="body table-responsive">
+                           <table class="table sicut-table-bordered sicut-modal-table1">
+                             <thead>
+                                 </thead><caption scope="row" class="sicut-tabla-titulo">Planta de agua</caption>
+                             <tbody>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 1</th>
+                                     <td>13134</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 2</th>
+                                     <td>13328</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 3</th>
+                                     <td>13084</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 4</th>
+                                     <td>13182</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel Bajo TK-100</th>
+                                     <td>13134</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel Alto TK-100</th>
+                                     <td>13328</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel Bajo TK-101</th>
+                                     <td>13084</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Nivel Alto TK-101</th>
+                                     <td>13182</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                       </div>
+                     </div>
+
+                     <div class="col-md-4">
+                       <div class="body table-responsive">
+                           <table class="table sicut-table-bordered sicut-modal-table1">
+                             <thead>
+                                 </thead><caption scope="row" class="sicut-tabla-titulo">Dinamometro</caption>
+                             <tbody>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 601</th>
+                                     <td>13134</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 602</th>
+                                     <td>13328</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 603</th>
+                                     <td>13084</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Bomba 604</th>
+                                     <td>13182</td>
+                                 </tr>
+                                  <tr>
+                                     <th class="sicut-th">Bomba 605</th>
+                                     <td>13182</td>
+                                 </tr>
+                                  <tr>
+                                     <th class="sicut-th">Bomba 606</th>
+                                     <td>13182</td>
+                                 </tr>
+                                  <tr>
+                                     <th class="sicut-th">Bomba 607</th>
+                                     <td>13182</td>
+                                 </tr>
+                                  <tr>
+                                     <th class="sicut-th">Bomba 608</th>
+                                     <td>13182</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Inundación sala 1</th>
+                                     <td>13084</td>
+                                 </tr>
+                                 <tr>
+                                     <th class="sicut-th">Inundación sala 2</th>
+                                     <td>13182</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                       </div>
+                     </div>
+
+
+
+                     <div class="col-md-12" style="margin-top: 20px">
+                       <div class="body table-responsive">
+                           <table class="table sicut-table-bordered sicut-modal-table1">
+                             <thead align="center">
+                                 <td>Fecha Hora</td>
+                                 <td>Nivel Bajo</td>
+                                 <td>Nivel Alto pozo</td>
+                                 <td>Nivel Alto</td>
+                                 <td>Nivel Alto TK</td>
+                             <tbody>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                                 <tr>
+                                     <td>Dato 1</td>
+                                     <td>Dato 2</td>
+                                     <td>Dato 3</td>
+                                     <td>Dato 4</td>
+                                     <td>Dato 5</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                       </div>
+                     </div>
+
+                  </div>
+                  <div role="tabpanel" class="tab-pane fade" id="profile">
+                     <div id="MaitenalParametros"></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="modal-footer">
+            <div id="grafico_maitenal"></div>
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+         </div>
+      </div>
+   </div>
+</div>
+<style>
+   div .btn-bomba{
+   color: #2b982b !important;
+   font-size: 13px;
+   }
+   .boton-bombas{
+   width: 9px;
+   height: 16px;
+   }            
+   .tabla-titulo{
+   background: #cccccc;
+   border: 1px solid #cccccc;
+   border-bottom: 0;
+   text-align: center; 
+   font-weight: 600; 
+   color: black;
+   padding-top: 4px;
+   padding-bottom: 4px;
+   border-top-left-radius: 10px;
+   border-top-right-radius: 10px;
+   }
+   .nombre-instalacion{
+   text-align: left;
+   }
+   .modal-table1 tbody tr th{
+   padding-top: 0px !important;
+   padding-bottom: 0px !important;
+   padding-right: 0px !important;
+   padding-left: 10px !important;
+   text-align: left;
+   }
+   .modal-table1 tbody tr td{
+   text-align: center;
+   padding: 0px !important;
+   }
+   .table-bordered tbody tr td, .table-bordered tbody tr th {
+   font-size: 13px;
+   border-color: #cccccc;
+   }
+   .col-1-5{
+   width: 20%;
+   float: left;
+   position: relative;
+   min-height: 1px;
+   padding-right: 15px;
+   padding-left: 15px;
+   }
+   .table tbody tr td, .table tbody tr th {
+   padding: 5px;
+   }
+   .table-custom table{
+   font-size: 13px;
+   }
+</style>
+<script  src="instalaciones/PlantaLican.js"></script>
+
+
+<script>
+      $( ".display-modal" ).click();
+   $(".loader-insta").css("display", "none");
+   
+            
+</script>
--- routes/web.php
+++ routes/web.php
@@ -135,3 +135,7 @@ Route::post('MaitenalController','MaitenalController@index')->middleware("verifi
 Route::get('MaitenalBombas','MaitenalController@MostrarBombas')->middleware("verificar_login");
 Route::get('MaitenalGrafico','MaitenalController@MaitenalGrafico')->middleware("verificar_login");
 Route::get('MaitenalParametros','MaitenalController@MaitenalParametros')->middleware("verificar_login");
+
+
+
+Route::post('FinningController','FinningController@index')->middleware("verificar_login");
