a8035ca6bb626085ff261ce171b34bd021c81f71
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2019-09-27 08:40:23 -0400

    Cambios realizados

--- app/Http/Controllers/TareasProgramadas/SincronizacionBDController.php
+++ app/Http/Controllers/TareasProgramadas/SincronizacionBDController.php
@@ -11,21 +11,23 @@ class SincronizacionBDController extends Controller
 {
     public function log_aasa()
     {
-    	$UltimaFecha = DB::connection("telemetria_local")->select("SELECT * FROM log_aasa ORDER BY mt_time DESC LIMIT 1");
+    	$Fechas = DB::connection("telemetria")->select("SELECT DISTINCT mt_time FROM log_aasa ORDER BY mt_time DESC LIMIT 3");
 
-    	DB::connection("telemetria_local")->select("DELETE FROM log_aasa WHERE mt_time='".$UltimaFecha[0]->mt_time."' ");
+    	$InsertarDatos0=DB::connection("telemetria")->select("SELECT * FROM log_aasa WHERE mt_time='".$Fechas[0]->mt_time."'");
+    	$InsertarDatos1=DB::connection("telemetria")->select("SELECT * FROM log_aasa WHERE mt_time='".$Fechas[1]->mt_time."'");
+    	$InsertarDatos2=DB::connection("telemetria")->select("SELECT * FROM log_aasa WHERE mt_time='".$Fechas[2]->mt_time."'");
+
+  		DB::connection("telemetria_local")->select("DELETE FROM log_aasa");
 
 
-		$InsertarDatos = DB::connection("telemetria")->select("SELECT * FROM log_aasa WHERE mt_time>='".$UltimaFecha[0]->mt_time."' ORDER BY mt_time DESC ");
+  		$InsertarDatos0=json_decode(json_encode($InsertarDatos0), true);
+  		$InsertarDatos1=json_decode(json_encode($InsertarDatos1), true);
+  		$InsertarDatos2=json_decode(json_encode($InsertarDatos2), true);
 
-		$InsertarDatos=json_decode(json_encode($InsertarDatos), true);
 
-		DB::connection("telemetria_local")->table("log_aasa")->insert($InsertarDatos);
-
-		
-		$UltimaFechaEliminar = DB::connection("telemetria_local")->select("SELECT * FROM log_aasa ORDER BY mt_time ASC LIMIT 1");
-
-		DB::connection("telemetria_local")->select("DELETE FROM log_aasa WHERE mt_time='".$UltimaFechaEliminar[0]->mt_time."' ");
+		DB::connection("telemetria_local")->table("log_aasa")->insert($InsertarDatos0);
+		DB::connection("telemetria_local")->table("log_aasa")->insert($InsertarDatos1);
+		DB::connection("telemetria_local")->table("log_aasa")->insert($InsertarDatos2);
 
 		return "Exito";
 		
